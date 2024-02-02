ed3f88035fd9398a32dcbe3bee0d16bb81a7e412
Thomas Johnson <tbjohnson123@gmail.com>
2024-02-02 13:10:10 -0800

    Fix import path join

--- src/scorper/private/mimport.nim
+++ src/scorper/private/mimport.nim
@@ -4,7 +4,8 @@ import os
 const explicitSourcePath {.strdefine.} = os.parentDir(os.parentDir( os.getCurrentCompilerExe()))
 
 macro mImport*(path: static[string]): untyped =
+  let combinedPath = joinPath(explicitSourcePath, path)
   result = newNimNode(nnkStmtList)
   result.add(quote do:
-    import `explicitSourcePath` /  `path`
+    import `combinedPath`
   )
